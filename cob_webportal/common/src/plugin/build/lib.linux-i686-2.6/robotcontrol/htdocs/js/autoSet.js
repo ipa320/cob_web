55c6d30aad180504c39c5e9aa5325c7e709b11c2
Simon Ebner <Simon.Ebner@gmail.com>
2013-01-16 12:08:53 +0100

    Remove build files

--- cob_webportal/common/src/plugin/build/lib.linux-i686-2.6/robotcontrol/htdocs/js/autoSet.js
+++
@@ -1,9 +0,0 @@
-$.fn.autoSet = function(obj) {
-
-	autoSet = obj.autoSet();
-	for (key in autoSet) {
-		value = autoSet[key];
-		this.find(".autoSet_" + key).text(value);
-	}
-
-};
--- cob_webportal/common/src/plugin/build/lib.linux-i686-2.6/robotcontrol/htdocs/js/infoBoxView.js
+++
@@ -1,3 +0,0 @@
-$.fn.renderInfoBox = function(username, status, args) {
-	this.html('<table><tr><th>User:</th><td>' + username + '</tr><th>Status:</th><td>' + status + '</td></tr><tr><th colspan="2"><a href="#" onclick="javascript:application.showServerReservation()">Server Reservation</a></th></tr></table>')
-}
