39a6d724f22c24d421236840a4291e8b4ccfb0b3
Simon Ebner <Simon.Ebner@gmail.com>
2013-07-11 02:38:15 +0200

    Correct path

--- cob_webportal/common/src/robotcontrold/startServer.bash
+++ cob_webportal/common/src/robotcontrold/startServer.bash
@@ -4,4 +4,4 @@ if screen -ls | grep robotcontrold > /dev/null; then
     echo 'If not kill the screen "robotcontrold"'
     exit
 fi
-screen -S robotcontrold -d -m sudo "$(dirname $0)/robotcontrold" --trac=/usr/local/trac/webportal2 --webPort=81 --timeout=3600 --logLevel=debug --console  --log=log.txt --sqliteDb=videodemo.sqlite
+screen -S robotcontrold -d -m sudo "$(dirname $0)/robotcontrold" --trac=/usr/local/trac/webportal --webPort=81 --timeout=3600 --logLevel=debug --console  --log=log.txt --sqliteDb=videodemo.sqlite
